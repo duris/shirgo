6ebee880a47e8284accbad474d356127ae1eea73
Ross Duris <rduris@Rosss-MacBook-Pro.local>
2014-08-29 21:08:12 -0400

    Initial Commit

---
+++ Pillow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pillow
+//
+//  Created by Ross Duris on 8/29/14.
+//  Copyright (c) 2014 Ross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PillowTests/PillowTests.m
@@ -0,0 +1,34 @@
+//
+//  PillowTests.m
+//  PillowTests
+//
+//  Created by Ross Duris on 8/29/14.
+//  Copyright (c) 2014 Ross. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PillowTests : XCTestCase
+
+@end
+
+@implementation PillowTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
